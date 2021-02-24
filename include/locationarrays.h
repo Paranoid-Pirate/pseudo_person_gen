8950eb5aa39b1dddedb30c7e69c2e9facd0eaad9
Paranoid-Pirate <captaingithub.supramarine@aleeas.com>
2021-02-24 13:38:29 -0500

    ok

--- include/locationarrays.h
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <vector>
-#pragma once
-
-struct locationArrays
-{
-    const std::vector <std::string> countryArray = {"Britain", "United States of America", "Switzerland", "Russia"};
-    const std::vector <std::string> stateArray = {"Maryland", "New York", "Wymoning"};
-};
--- include/traitarrays.h
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include <vector>
-#pragma once
-
-struct traitArray
-{
-    const std::vector <std::string> traitArray {"likes dogs", "Likes cats", "sucks lmao", "tired"};
-};
